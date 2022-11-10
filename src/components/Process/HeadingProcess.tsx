866ad6f3da13f4eacd25b3a54d6aa93d14919af3
Masoud <78568883+masoodhesami@users.noreply.github.com>
2022-11-10 18:41:19 +0330

    part-1 process section done

---
+++ src/components/Process/HeadingProcess.tsx
@@ -0,0 +1,8 @@
+const HeadingProcess = () => {
+    return (
+        <h2 className="mb-4 text-xl text-gray-900 md:text-2xl lg:text-3xl"> The Process <br />
+            About Our Work
+        </h2>
+    )
+}
+export default HeadingProcess;
